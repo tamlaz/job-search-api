1adc90329e38036661ecbb355b83957b1f8334ef
Tamas Lazar <lazartamas97@gmail.com>
2023-10-14 22:13:53 +0200

    Implement query that search in database by keyword and location

--- src/main/java/com/lazartamas/jobsearchapi/repository/PositionRepository.java
+++ src/main/java/com/lazartamas/jobsearchapi/repository/PositionRepository.java
@@ -1,7 +1,16 @@
 package com.lazartamas.jobsearchapi.repository;
 
 import com.lazartamas.jobsearchapi.domain.Position;
+import com.lazartamas.jobsearchapi.dto.outgoing.PositionListItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PositionRepository extends JpaRepository<Position, Long> {
+
+    @Query("SELECT p FROM Position p WHERE LOWER(p.jobTitle) LIKE LOWER(CONCAT('%', :keyword, '%'))" +
+            " AND LOWER(p.location) LIKE LOWER(CONCAT('%', :location, '%')) ")
+    List<PositionListItem> findByKeywordAndLocation(@Param("keyword") String keyword, @Param("location") String location);
 }
