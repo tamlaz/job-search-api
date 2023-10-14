38188d00dcb10a34c93c37c1a069b949c012462b
Tamas Lazar <lazartamas97@gmail.com>
2023-10-14 19:41:49 +0200

    Implement client registration method that returns with an apikey

--- src/main/java/com/lazartamas/jobsearchapi/service/ClientService.java
+++ src/main/java/com/lazartamas/jobsearchapi/service/ClientService.java
@@ -1,9 +1,28 @@
 package com.lazartamas.jobsearchapi.service;
 
+import com.lazartamas.jobsearchapi.domain.Client;
+import com.lazartamas.jobsearchapi.dto.incoming.ClientRegistrationFormData;
+import com.lazartamas.jobsearchapi.repository.ClientRepository;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class ClientService {
+
+    private final ClientRepository clientRepository;
+
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public String registerClient(ClientRegistrationFormData formData) {
+        Client clientToSave = new Client(formData);
+        clientRepository.save(clientToSave);
+        return clientToSave.getApiKey();
+    }
+
+
 }
