94a5cb9afcd2e80080b68603fe6170a1b6e93e98
Tamas Lazar <lazartamas97@gmail.com>
2023-10-14 20:42:26 +0200

    Add PostMethod for creating positions, bind validator to PositionFormData

--- src/main/java/com/lazartamas/jobsearchapi/controller/PositionController.java
+++ src/main/java/com/lazartamas/jobsearchapi/controller/PositionController.java
@@ -1,9 +1,29 @@
 package com.lazartamas.jobsearchapi.controller;
 
+import com.lazartamas.jobsearchapi.dto.incoming.PositionFormData;
+import com.lazartamas.jobsearchapi.service.PositionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/positions")
 public class PositionController {
+
+    private final PositionService positionService;
+
+    public PositionController(PositionService positionService) {
+        this.positionService = positionService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<String> createPosition(@RequestBody @Validated PositionFormData formData) {
+        String positionUrl = positionService.createPosition(formData);
+        return new ResponseEntity<>(positionUrl, HttpStatus.CREATED);
+    }
 }
