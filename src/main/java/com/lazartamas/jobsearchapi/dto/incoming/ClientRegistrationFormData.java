c7207912652b93afdd2b31be27c6deba94e860f2
Tamas Lazar <lazartamas97@gmail.com>
2023-10-14 19:38:26 +0200

    Create dto for client registration

---
+++ src/main/java/com/lazartamas/jobsearchapi/dto/incoming/ClientRegistrationFormData.java
@@ -0,0 +1,10 @@
+package com.lazartamas.jobsearchapi.dto.incoming;
+
+import lombok.Data;
+
+@Data
+public class ClientRegistrationFormData {
+
+    private String name;
+    private String email;
+}
