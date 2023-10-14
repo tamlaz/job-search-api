eec312d10868545da70405da916a01a92dbb9d9f
Tamas Lazar <lazartamas97@gmail.com>
2023-10-14 23:07:53 +0200

    Add JsonIgnoreProperties to map results from reed api to PositionListItem

--- src/main/java/com/lazartamas/jobsearchapi/dto/outgoing/PositionListItem.java
+++ src/main/java/com/lazartamas/jobsearchapi/dto/outgoing/PositionListItem.java
@@ -1,20 +1,22 @@
 package com.lazartamas.jobsearchapi.dto.outgoing;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.lazartamas.jobsearchapi.domain.Position;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PositionListItem {
 
     private String jobTitle;
-    private String location;
+    private String locationName;
     private String jobUrl;
 
     public PositionListItem(Position position) {
         this.jobTitle = position.getJobTitle();
-        this.location = position.getLocation();
+        this.locationName = position.getLocation();
         this.jobUrl = position.getUrl();
     }
 }
