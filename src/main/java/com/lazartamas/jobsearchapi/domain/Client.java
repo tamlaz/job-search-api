7fba2b28f245d2273f18237869179f7e26d2f5b5
Tamas Lazar <lazartamas97@gmail.com>
2023-10-14 19:37:03 +0200

    Create constructor that expects a ClientRegistrationFormData dto as parameter

--- src/main/java/com/lazartamas/jobsearchapi/domain/Client.java
+++ src/main/java/com/lazartamas/jobsearchapi/domain/Client.java
@@ -1,10 +1,15 @@
 package com.lazartamas.jobsearchapi.domain;
 
+import com.lazartamas.jobsearchapi.dto.incoming.ClientRegistrationFormData;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 @Entity
 @Data
+@NoArgsConstructor
 public class Client {
 
     @Id
@@ -20,4 +25,10 @@ public class Client {
 
     @Column
     private String apiKey;
+
+    public Client(ClientRegistrationFormData formData) {
+        this.name = formData.getName();
+        this.email = formData.getEmail();
+        this.apiKey = UUID.randomUUID().toString();
+    }
 }
