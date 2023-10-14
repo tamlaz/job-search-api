ffb57285024e33b97a2756b38995b5cdd461829a
Tamas Lazar <lazartamas97@gmail.com>
2023-10-14 19:40:56 +0200

    Implement method to find client by email address

--- src/main/java/com/lazartamas/jobsearchapi/repository/ClientRepository.java
+++ src/main/java/com/lazartamas/jobsearchapi/repository/ClientRepository.java
@@ -2,8 +2,15 @@ package com.lazartamas.jobsearchapi.repository;
 
 import com.lazartamas.jobsearchapi.domain.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    @Query("SELECT c FROM Client c where c.email=:email")
+    Optional<Client> findClientByEmail(@Param("email") String param);
 }
