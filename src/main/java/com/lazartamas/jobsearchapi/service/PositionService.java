5b437b18c09f0e30c45fb5ab4defff396947580c
Tamas Lazar <lazartamas97@gmail.com>
2023-10-14 22:13:10 +0200

    Create method that finds positions by keyword and location in database

--- src/main/java/com/lazartamas/jobsearchapi/service/PositionService.java
+++ src/main/java/com/lazartamas/jobsearchapi/service/PositionService.java
@@ -2,19 +2,42 @@ package com.lazartamas.jobsearchapi.service;
 
 import com.lazartamas.jobsearchapi.domain.Position;
 import com.lazartamas.jobsearchapi.dto.incoming.PositionFormData;
+import com.lazartamas.jobsearchapi.dto.outgoing.PositionListItem;
 import com.lazartamas.jobsearchapi.repository.PositionRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
 
 @Service
 @Transactional
 public class PositionService {
 
     private final PositionRepository positionRepository;
+    private WebClient webClient;
+
+    @Value("${reed-jobs-url}")
+    private String jobsUrl;
+
+    @Value("${reed-jobs-apikey}")
+    private String apiKey;
+
     private final String BASE_URL = "http://localhost:8080/positions";
 
     public PositionService(PositionRepository positionRepository) {
         this.positionRepository = positionRepository;
+    }
+
+    @PostConstruct
+    public void init() {
+        this.webClient = WebClient.builder()
+                .baseUrl(jobsUrl)
+                .filter(ExchangeFilterFunctions.basicAuthentication(apiKey, ""))
+                .build();
     }
 
     public String createPosition(PositionFormData formData) {
@@ -27,4 +50,12 @@ public class PositionService {
     protected String generateUrl(Long id) {
         return BASE_URL + "/" + id;
     }
+
+    public List<PositionListItem> searchJobsByKeywordAndLocation(PositionFormData positionFormData) {
+        List<PositionListItem> jobsInDatabase = positionRepository
+                .findByKeywordAndLocation(positionFormData.getJobTitle(), positionFormData.getLocation());
+        return jobsInDatabase;
+    }
+
+
 }
