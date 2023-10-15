cd619b38994d4f9e4718640e12e443e9db470ee3
Tamas Lazar <lazartamas97@gmail.com>
2023-10-15 22:22:01 +0200

    Use MessageSource as a bean

---
+++ src/main/java/com/lazartamas/jobsearchapi/configuration/ApplicationConfig.java
@@ -0,0 +1,18 @@
+package com.lazartamas.jobsearchapi.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean()
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setCacheSeconds(10);
+        return messageSource;
+    }
+}
