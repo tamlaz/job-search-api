1402a7ee1350a3b8b2c33ec76c123e373e329862
Tamas Lazar <lazartamas97@gmail.com>
2023-10-15 22:20:55 +0200

    Delete redundant dependency

--- src/main/java/com/lazartamas/jobsearchapi/validator/PositionFormDataValidator.java
+++ src/main/java/com/lazartamas/jobsearchapi/validator/PositionFormDataValidator.java
@@ -1,19 +1,12 @@
 package com.lazartamas.jobsearchapi.validator;
 
 import com.lazartamas.jobsearchapi.dto.incoming.PositionFormData;
-import com.lazartamas.jobsearchapi.repository.PositionRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 @Component
 public class PositionFormDataValidator implements Validator {
-
-    private final PositionRepository positionRepository;
-
-    public PositionFormDataValidator(PositionRepository positionRepository) {
-        this.positionRepository = positionRepository;
-    }
 
     @Override
     public boolean supports(Class<?> clazz) {
